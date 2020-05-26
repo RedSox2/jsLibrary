421519fbc40ded86df076e16845ff42f77c77d0e
RedSox2 <65141925+RedSox2@users.noreply.github.com>
2020-05-26 09:18:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var findReplace = function(str a, str b) {
 	var find = docmuent.getElementById(str a);
 	var replace = str b;
 
-	find.textContent.innerHTML = replace; 
+	find.innerHTML = replace; 
 };
